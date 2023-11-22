a1c782ee4e9e782c490cba5d34f21e33e541dc80
Ananiya Belew <ananiya@arifpay.org>
2023-11-22 16:08:06 +0300

    final

--- Arifpay.js
+++ Arifpay.js
@@ -68,39 +68,3 @@ class ArifPay{
 }
 }
 module.exports=ArifPay;
-const arifPay = new ArifPay("G8FbER8zZ9uco5tLuVnNKycJwXzvJTyo","2025-02-01T03:45:27");
-const paymentInfo = {
-    cancelUrl: "https://example.com",
-    errorUrl: "http://error.com",
-    notifyUrl: "https://gateway.arifpay.net/test/callback",
-    successUrl: "http://example.com",
-    paymentMethods: [
-    ],
-    expireDate: "2025-02-01T03:45:27",
-    items: [
-        {
-            "name": "ሙዝ",
-            "quantity": 1,
-            "price": 1,
-            "description": "Fresh Corner preimuim Banana.",
-            "image": "https://4.imimg.com/data4/KK/KK/GLADMIN-/product-8789_bananas_golden-500x500.jpg"
-        },
-        {
-            "name": "ሙዝ",
-            "quantity": 1,
-            "price": 1,
-            "description": "Fresh Corner preimuim Banana.",
-            "image": "https://4.imimg.com/data4/KK/KK/GLADMIN-/product-8789_bananas_golden-500x500.jpg"
-        }
-    ],
-    beneficiaries: [
-        {
-            "accountNumber": "01320811436100",
-            "bank": "AWINETAA",
-            "amount": 2.0
-        }
-    ],
-    lang: "EN"
-};
-
-arifPay.Make_payment(paymentInfo).then(res=>console.log(res))
